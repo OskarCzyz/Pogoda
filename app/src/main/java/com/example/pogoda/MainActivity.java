452121f4d426a79bcbaa264507d389c2d07ff7ba
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-10-23 21:41:52 +0200

    aktualizacja mapki, usuniecie status bar

--- app/src/main/java/com/example/pogoda/MainActivity.java
+++ app/src/main/java/com/example/pogoda/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.pogoda;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
