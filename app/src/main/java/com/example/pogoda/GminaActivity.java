9e2ac91b4f816209ac3fb06a56b611ced76a023f
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-10-23 23:09:21 +0200

    proste api

--- app/src/main/java/com/example/pogoda/GminaActivity.java
+++ app/src/main/java/com/example/pogoda/GminaActivity.java
@@ -1,14 +1,75 @@
 package com.example.pogoda;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class GminaActivity extends AppCompatActivity {
 
+    private TextView tvTitle;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gmina);
+
+        tvTitle = findViewById(R.id.tvTitle);
+
+        String url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&timezone=auto&forecast_days=1";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject current = response.getJSONObject("current");
+                    String text = (String) current.getString("time") + "\n" + current.getString("temperature_2m");
+                    tvTitle.setText(text);
+                } catch (JSONException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tvTitle.setText("error ");
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/pogoda/MainActivity.java
+++ app/src/main/java/com/example/pogoda/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.pogoda;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvBrenna, tvCieszyn, tvZebrzydowice, tvStrumien, tvChybie, tvHazlach, tvDebowiec, tvSkoczow,tvGoleszow,tvUstron,tvWisla,tvIstebna;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,5 +29,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        tvBrenna = findViewById(R.id.tvBrenna);
+        tvCieszyn = findViewById(R.id.tvCieszyn);
+        tvZebrzydowice = findViewById(R.id.tvZebrzydowice);
+        tvStrumien = findViewById(R.id.tvStrumien);
+        tvChybie = findViewById(R.id.tvChybie);
+        tvHazlach = findViewById(R.id.tvHazlach);
+        tvDebowiec = findViewById(R.id.tvDebowiec);
+        tvSkoczow = findViewById(R.id.tvSkoczow);
+        tvGoleszow = findViewById(R.id.tvGoleszow);
+        tvUstron = findViewById(R.id.tvUstron);
+        tvWisla = findViewById(R.id.tvWisla);
+        tvIstebna = findViewById(R.id.tvIstebna);
+
+        tvBrenna.setOnClickListener(MainActivity.this);
+        tvCieszyn.setOnClickListener(MainActivity.this);
+        tvStrumien.setOnClickListener(MainActivity.this);
+        tvChybie.setOnClickListener(MainActivity.this);
+        tvHazlach.setOnClickListener(MainActivity.this);
+        tvDebowiec.setOnClickListener(MainActivity.this);
+        tvSkoczow.setOnClickListener(MainActivity.this);
+        tvGoleszow.setOnClickListener(MainActivity.this);
+        tvUstron.setOnClickListener(MainActivity.this);
+        tvWisla.setOnClickListener(MainActivity.this);
+        tvIstebna.setOnClickListener(MainActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView text = (TextView) v;
+        String nazwa = (String) text.getText();
+
+        Intent intent = new Intent(MainActivity.this, GminaActivity.class);
+        intent.putExtra("Powiat", nazwa);
+        startActivity(intent);
     }
 }
