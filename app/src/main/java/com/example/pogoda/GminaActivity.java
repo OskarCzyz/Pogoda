28fd1747765ac3817f427dd59c988f97872e2331
OskarCzyz <49710785+OskarCzyz@users.noreply.github.com>
2023-11-05 20:15:50 +0100

    temperatura pokazywana jest na podstawie powiatu, którego się wybrało, obrazek w drugiej aktywności jest zależny od aktualnej pogody małe poprawki layout'u

--- app/src/main/java/com/example/pogoda/GminaActivity.java
+++ app/src/main/java/com/example/pogoda/GminaActivity.java
@@ -7,6 +7,9 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -25,11 +28,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GminaActivity extends AppCompatActivity {
 
     private TextView tvTitle;
+
+    private ImageView ivStatus;
+
+    private ListView listView;
+
+    private List<String> temperatures = new ArrayList<>();
 
 
 
@@ -39,8 +50,7 @@ public class GminaActivity extends AppCompatActivity {
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
+// Hide the action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
@@ -48,8 +58,14 @@ public class GminaActivity extends AppCompatActivity {
         setContentView(R.layout.gmina);
 
         tvTitle = findViewById(R.id.tvTitle);
+        ivStatus = findViewById(R.id.ivStatus);
+        listView = findViewById(R.id.listView);
 
-        String url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&timezone=auto&forecast_days=1";
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(GminaActivity.this, android.R.layout.simple_list_item_1, temperatures);
+        listView.setAdapter(adapter);
+
+
+        String url = "https://api.open-meteo.com/v1/forecast?latitude=" + getIntent().getDoubleExtra("Latitude", 0) + "&longitude="+ getIntent().getDoubleExtra("Longitude", 0)+"&current=is_day,rain,showers,weather_code&hourly=temperature_2m&timezone=auto&forecast_days=1";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
             @SuppressLint("SetTextI18n")
@@ -57,8 +73,47 @@ public class GminaActivity extends AppCompatActivity {
             public void onResponse(JSONObject response) {
                 try {
                     JSONObject current = response.getJSONObject("current");
-                    String text = (String) current.getString("time") + "\n" + current.getString("temperature_2m");
-                    tvTitle.setText(text);
+                    switch (current.getInt("weather_code")) {
+                        case 0:
+                        case 1: ivStatus.setImageResource(R.drawable.clear_status); break;
+                        case 2:
+                        case 3: ivStatus.setImageResource(R.drawable.cloudy_status); break;
+                        case 45:
+                        case 48: ivStatus.setImageResource(R.drawable.foggy_status); break;
+                        case 51:
+                        case 53:
+                        case 55:
+                        case 56:
+                        case 57:
+                        case 61:
+                        case 63:
+                        case 65:
+                        case 66:
+                        case 67: ivStatus.setImageResource(R.drawable.rain_status); break;
+                        case 71:
+                        case 73:
+                        case 75:
+                        case 85:
+                        case 86:
+                        case 77: ivStatus.setImageResource(R.drawable.snow_status); break;
+                        case 80:
+                        case 81:
+                        case 82: ivStatus.setImageResource(R.drawable.rainshowers_status); break;
+                        case 95:
+                        case 96:
+                        case 99: ivStatus.setImageResource(R.drawable.thunderstorm_status); break;
+                        default: break;
+                    }
+
+                    tvTitle.setText(getIntent().getStringExtra("Name"));
+
+                    JSONObject hourly = response.getJSONObject("hourly");
+                    for (int i =0; i < 24; i++) {
+                        temperatures.add(hourly.getJSONArray("time").getString(i) + " " + hourly.getJSONArray("temperature_2m").getString(i));
+                    }
+                    adapter.notifyDataSetChanged();
+
+
                 } catch (JSONException e ) {
                     e.printStackTrace();
                 }
--- app/src/main/java/com/example/pogoda/MainActivity.java
+++ app/src/main/java/com/example/pogoda/MainActivity.java
@@ -53,16 +53,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvUstron.setOnClickListener(MainActivity.this);
         tvWisla.setOnClickListener(MainActivity.this);
         tvIstebna.setOnClickListener(MainActivity.this);
+        tvZebrzydowice.setOnClickListener(MainActivity.this);
 
     }
 
     @Override
     public void onClick(View v) {
         TextView text = (TextView) v;
-        String nazwa = (String) text.getText();
+        String name = (String) text.getText();
+
+        double latitude = 0, longitude =0;
+        if (name.equals("Brenna")) {
+            latitude = 49.7258;
+            longitude = 18.9025;
+        } else if (name.equals("Chybie")) {
+            latitude = 49.9025;
+            longitude = 18.8276;
+        } else if (name.equals("Cieszyn")) {
+            latitude = 49.7513;
+            longitude = 18.6321;
+        } else if (name.equals("Goleszów")) {
+            latitude = 49.7358;
+            longitude = 18.7368;
+        } else if (name.equals("Haźlach")) {
+            latitude = 49.8071;
+            longitude = 18.6518;
+        } else if (name.equals("Istebna")) {
+            latitude = 49.5632;
+            longitude = 18.9057;
+        } else if (name.equals("Skoczów")) {
+            latitude = 49.8009;
+            longitude = 18.7877;
+        } else if (name.equals("Strumień")) {
+            latitude = 49.9210;
+            longitude = 18.7664;
+        } else if (name.equals("Ustroń")) {
+            latitude = 49.7215;
+            longitude = 18.8020;
+        } else if (name.equals("Wisła")) {
+            latitude = 49.6563;
+            longitude = 18.8591;
+        } else if (name.equals("Zebrzydowice")) {
+            latitude = 49.8779;
+            longitude = 18.6113;
+        }
 
         Intent intent = new Intent(MainActivity.this, GminaActivity.class);
-        intent.putExtra("Powiat", nazwa);
+        intent.putExtra("Name", name);
+        intent.putExtra("Latitude", latitude);
+        intent.putExtra("Longitude", longitude);
         startActivity(intent);
     }
 }
